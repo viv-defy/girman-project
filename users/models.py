4d190aa225055fff4662905acefc2a344e88ff0d
viv-defy <vish.eth15@gmail.com>
2024-12-03 21:56:56 +0530

    feat: remove role-description. upload roles through migration

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Role(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True, null=True)
     permissions = models.ManyToManyField("Permission", related_name='roles', blank=True)
 
     def __str__(self):
