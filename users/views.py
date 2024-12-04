fa22f157c449c84dd88b5224c5808111fa160473
viv-defy <vish.eth15@gmail.com>
2024-12-04 11:00:16 +0530

    feat: add dummy urls and views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 
-# Create your views here.
+
+@api_view(["POST"])
+@permission_classes([IsAdminUser])
+def add_user(request):
+    """
+    Add a new user
+    """
+
+    return Response(
+        {
+            "success": True,
+            "message": "User data",
+            "data": {"user": {}}
+        },
+        status=HTTP_201_CREATED
+    )
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    """
+    Get a list of users
+    """
+
+    return Response(
+        {
+            "success": True,
+            "message": "Users list",
+            "data": {}
+        },
+        status=HTTP_200_OK
+    )
+
+
+@api_view(["PATCH"])
+@permission_classes([IsAdminUser])
+def assign_permission(request):
+    """
+    Assign user permissions
+    """
+
+    return Response(
+        {
+            "success": True,
+            "message": "Updated user data",
+            "data": {"user": {}}
+        },
+        status=HTTP_200_OK
+    )
+    
