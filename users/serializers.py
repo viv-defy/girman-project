8a30aea5598491b9e99e7dede246bebca73b6864
viv-defy <vish.eth15@gmail.com>
2024-12-04 12:28:04 +0530

    fix: add email to user serializer and create user end point

--- users/serializers.py
+++ users/serializers.py
@@ -18,4 +18,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "is_active", "created_at", "updated_at", "roles"]
+        fields = ["id", "username", "email", "is_active", "created_at", "updated_at", "roles"]
